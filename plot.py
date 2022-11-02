8a32ef173935c1f35f3459a4d0feddb167c0636a
Jericho-Coder <jerichobondi501@gmail.com>
2022-11-01 21:31:02 -0600

    \

---
+++ plot.py
@@ -0,0 +1,39 @@
+from rich import *
+
+global raw
+global numbers
+numbers = []
+
+raw = open("raw.txt", "a")
+
+def solve():
+    steps = int(0)
+    num = int(input('Input a Positive Integer: '))
+    while num != 1:
+        numbers.append(num)
+        if num % 2 == 0:
+            num = int(num / 2)
+            print("[red]" + str(num) + " Number is Odd ")
+            steps = steps + 1
+        else:
+            num = int(3 * num + 1)
+            print("[green]" + str(num) + " Number is Even")
+            steps = steps + 1
+    else:
+        numbers.append(num)
+        print('Finished! Problem Took ' + str(steps) + " Steps.")
+
+solve()
+
+numbers.reverse()
+print(numbers)
+
+def plot():
+    steps = 0
+    for value in numbers:
+        raw.write("(" + str(value) + "," + str(steps) + "),")
+        steps = steps + 1
+
+plot()
+raw.write("\n")
+raw.close()
